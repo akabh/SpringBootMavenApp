bd75cdc653594cd9955be9f64762ec0ec6752046
Abhishek <abhishekimsec888@gmail.com>
2020-10-12 11:37:24 +0530

    commit By Abhishek

---
+++ spring-boot-crud-operation/src/test/java/com/nttdata/devops/training/SpringBootCrudOperationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nttdata.devops.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class SpringBootCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
